1fd5ccfadcf3b5ad13627de6b5ba918eafc24f00
Ксения Макарова <kseniamakarova@MacBook-Air-3.local>
2026-05-01 16:17:01 +0300

    deploy ar-mvp 2026-05-01 16:17

---
+++ _next/static/chunks/app/layout-63f14b04749153c7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1743:()=>{},2475:e=>{e.exports={style:{fontFamily:"'sirius', 'sirius Fallback'"},className:"__className_7e1d29",variable:"__variable_7e1d29"}},6069:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,2475,23)),Promise.resolve().then(a.t.bind(a,1743,23))}},e=>{e.O(0,[310,441,794,358],()=>e(e.s=6069)),_N_E=e.O()}]);
